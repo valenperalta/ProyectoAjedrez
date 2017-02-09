66d3518051fb4c53c4de2390039f654ec3e6131f
valentinam.peralta <valentinam.peralta@172.16.16.55>
2017-02-09 14:32:00 -0500

    test commit

--- src/proyectoajedrez/cTablero.java
+++ src/proyectoajedrez/cTablero.java
@@ -10,5 +10,36 @@ package proyectoajedrez;
  * @author valentinam.peralta
  */
 public class cTablero {
-    
+
+    public char[][] tablero;
+
+    public cTablero() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                tablero[i][j] = ' ';
+            }
+        }
+    }
+
+    public void setTablero() {
+        for (char i = 'a'; i < 'h'; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                tablero[i][j] = i;
+
+            }
+        }
+    }
+
+    public void gestionTablero() {
+
+    }
+
+    public void mostrarTablero() {
+        for (int row = 0; row < tablero.length; row++) {
+            for (int column = 0; column < tablero[row].length; column++) {
+                System.out.println(tablero[row][column] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
